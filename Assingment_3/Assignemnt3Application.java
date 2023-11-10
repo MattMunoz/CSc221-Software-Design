85f76db0ba8fd1d7b065612e8f9dbdf34586102e
MattMunoz <88506690+MattMunoz@users.noreply.github.com>
2023-11-09 21:11:24 -0500

    Asssingment_4 initial

---
+++ Assingment_3/Assignemnt3Application.java
@@ -0,0 +1,13 @@
+package com.example.assignemnt_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignemnt3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignemnt3Application.class, args);
+    }
+
+}
